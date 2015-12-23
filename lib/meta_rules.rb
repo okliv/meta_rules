b4ea0f863e640837574e898c17d8e4d60cc183cc
okliv <doktorlivesey@gmail.com>
2015-12-23 02:12:22 +0200

    Update meta_rules.rb

--- lib/meta_rules.rb
+++ lib/meta_rules.rb
@@ -97,7 +97,7 @@ module MetaRules
       @true_or_false_arr ||= url_get_params.map do |key, value| #id: 12
         rule_hash_values = [*(get_params.try(:[], key))].compact.presence
         # rule_hash_values.try(:include?, value)||
-        rule_hash_values.to_a.select{ |r| r[/#{value}/] }.size>0||rule_hash_values.try(:include?, Rule.any)&&!rule_hash_values.try(:include?, Rule.none)
+        rule_hash_values.to_a.select{ |r| r[/Regexp.new(#{value})/] }.size>0||rule_hash_values.try(:include?, Rule.any)&&!rule_hash_values.try(:include?, Rule.none)
       end.compact.presence||[true]
     end
 
